dec52fce9c9e42ca20b14a80189c35ee0a32672a
a-r-t <alexthimineur@gmail.com>
2023-02-14 19:40:11 -0500

    added variety of snacks/prices and a welcome message to the user

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,7 +1,10 @@
 public class VendingMachine {
     public static void main(String[]args)
     {
-        String[] snacks = new String[4];
+        String[] snacks = new String[] { "Doritos", "Flaming Hot Cheetos", "Skittles", "Cheez Its", "Gummy Bears" };
+        double[] prices = new double[] { 2.99, 2.99, 3.50, 2.50, 1.20 };
+        System.out.println("Welcome to the vending machine of your dreams!");
+        
     }
     
 }
