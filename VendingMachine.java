a7746620a60438ce479f1edf6552cce97dd50d7b
atoomer <atoomer@quinnipiac.com>
2023-02-14 19:50:56 -0500

    Aadded money input system. Available credit stored as double credit.

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 public class VendingMachine {
     public static void main(String[]args)
     {
         String[] snacks = new String[] { "Doritos", "Flaming Hot Cheetos", "Skittles", "Cheez Its", "Gummy Bears" };
         double[] prices = new double[] { 2.99, 2.99, 3.50, 2.50, 1.20 };
         System.out.println("Welcome to the vending machine of your dreams!");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please tell how much money you are inserting.");
+        double credit = scan.nextDouble(); 
+        System.out.println("$"+credit+" in credit available.");
         
     }
     
